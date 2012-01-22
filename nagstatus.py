6ae403af7d22196b85ee6920dd2652366373f69e
Brandon Whaley <redkrieg@gmail.com>
2012-01-22 00:26:31 -0500

    added some comments, handled a bug in parsing if "{" appeared in a value

--- nagstatus.py
+++ nagstatus.py
@@ -2,10 +2,11 @@
 
 def get_property(line):
     line_split = line.strip().split('=')
+    #returns the property name and remaining data recombined after the split above
     return line_split[0], '='.join(line_split[1:])
 
 def try_to_convert(value):
-    """Tries to convert [value] to an int, returns string on fail"""
+    """Tries to convert [value] to an int, returns the original string on fail"""
     try:
         return int(value)
     except:
@@ -26,10 +27,11 @@ def get_nag_status(file, threshold = 0):
     group_type = None
 
     while line:
-        if '{' in line:
+        if line.strip().endswith('{'):
             group_type = line.strip().split()[0]
         try:
-            property, value = get_property(line)
+            property, value = get_property(line) #fails on lines without =, the try makes us pass
+            #not yet reading programstatus or info
             if group_type == 'hoststatus':
                 if property == 'host_name':
                     this_host = value
@@ -38,6 +40,7 @@ def get_nag_status(file, threshold = 0):
                 else:
                     host_statuses[this_host]['HOST'][property] = try_to_convert(value)
             elif group_type == 'servicestatus':
+                #host_name always comes before service_description
                 if property == 'host_name':
                     this_host = value
                 elif property == 'service_description':
@@ -46,6 +49,7 @@ def get_nag_status(file, threshold = 0):
                 else:
                     host_statuses[this_host][this_service][property] = try_to_convert(value)
                     if property == 'current_state' and host_statuses[this_host][this_service][property] < threshold:
+                        #by simply removing the service here, subsequent attempts to add data fail to the next loop iteration
                         del host_statuses[this_host][this_service]
         except:
             pass
@@ -53,4 +57,5 @@ def get_nag_status(file, threshold = 0):
     return host_statuses
 
 if __name__ == "__main__":
+    #simply me testing, this chunk is not needed
     print get_nag_status('status.dat', 1)
