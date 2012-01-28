11566f1c0925e0f1cce0b7462137a3b79ea19283
Brandon Whaley <redkrieg@gmail.com>
2012-01-28 15:35:10 -0500

    exception handling for bad queries

--- nagdash.py
+++ nagdash.py
@@ -97,7 +97,10 @@ def init_db():
         db.commit()
 
 def query_db(query, args=(), one=False):
-    cur = g.db.execute(query, args)
+    try:
+        cur = g.db.execute(query, args)
+    except:
+        return None
     rv = [dict((cur.description[idx][0], value)
                for idx, value in enumerate(row)) for row in cur.fetchall()]
     return (rv[0] if rv else None) if one else rv
