70711ea1aae1288a307f5febed55be73fa43d5ed
Brandon Whaley <redkrieg@gmail.com>
2012-01-28 20:03:36 -0500

    stuck in login loop

--- nagdash.py
+++ nagdash.py
@@ -129,8 +129,8 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login(next = None):
     error = None
-    if next is None:
-        next = url_for('index')
+    #if next is None:
+    #    next = url_for('index')
     # Check if any users exist:
     userresult = query_db('select count(*) from users', one=True)
     if userresult is None or userresult['count(*)'] == 0:
