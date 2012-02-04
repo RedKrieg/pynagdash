78fafadb9d068d49f08d8d74c860efd2bd922412
Brandon Whaley <redkrieg@gmail.com>
2012-02-04 18:10:15 -0500

    sql problems

--- nagdash.sql
+++ nagdash.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `ADMIN` int(1) NOT NULL,
   `DISABLED` int(1) NOT NULL,
   PRIMARY KEY (`USER`)
-)
+);
 CREATE TABLE IF NOT EXISTS `views` (
   `NAME` varchar(64) NOT NULL,
   `DESCRIPTION` varchar(512) NOT NULL,
