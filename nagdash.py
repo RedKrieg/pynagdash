413d36b23eaf96f409d87f5021a91f3448609ac7
Brandon Whaley <redkrieg@gmail.com>
2012-02-04 20:10:22 -0500

    debugging time

--- nagdash.py
+++ nagdash.py
@@ -158,10 +158,10 @@ def init_db():
         db.commit()
 
 def query_db(query, args=(), one=False):
-    try:
-        cur = g.db.execute(query, args)
-    except:
-        return None
+    #try:
+    cur = g.db.execute(query, args)
+    #except:
+    #    return None
     rv = [dict((cur.description[idx][0], value)
                for idx, value in enumerate(row)) for row in cur.fetchall()]
     return (rv[0] if rv else None) if one else rv
