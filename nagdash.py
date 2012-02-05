fdb5694feb9c025ed8c61ced07f458f0521ca057
Brandon Whaley <redkrieg@gmail.com>
2012-02-04 19:54:30 -0500

    use dropdowns instead of checkboxes

--- nagdash.py
+++ nagdash.py
@@ -278,7 +278,7 @@ def edit_users(error=""):
         user_names = request.form.getlist('username')
         user_admins = request.form.getlist('admin')
         user_disableds = request.form.getlist('disabled')
-        error = (user_names, user_admins, user_disableds)
+        error = request.form
         user_list = [ {'USER': user, 'ADMIN': admin, 'DISABLED': disabled } for (user, admin, disabled) in zip(user_names, user_admins, user_disableds) ]
     else:
         user_list = list_users()
